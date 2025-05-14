8b9bd837256da83aeb27077f4620b22f249772be
jp-zuniga <jperez070705@gmail.com>
2025-05-14 10:20:55 -0600

    ejercicio de notas completados

---
+++ 14_5_25/ejercicio_matrices.py
@@ -0,0 +1,5 @@
+"""
+Programar una función que le permita al usuario ingresar una matriz de
+cualquier dimensión, y otras funciones que reciban dos matrices como parámetros,
+y realicen operaciones matemáticas sobre ellas (suma, resta, multiplicación).
+"""
---
+++ 14_5_25/ejercicio_notas.py
@@ -0,0 +1,4 @@
+"""
+Representar las notas de todas las clases de un grupo de estudiantes con matrices.
+Calcular los promedios de cada estudiante y de cada clase.
+"""
